fb863193fe30f22955943b94605ab99cbc7f7e86
Zachary <zbutler46@hotmail.com>
2023-06-05 17:36:34 -0400

    changes to SimpleStorage K and adding abi file

---
+++ demos/web3_py_simple_storage/deploy.py
@@ -0,0 +1,30 @@
+from solcx import compile_standard, install_solc
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+install_solc("0.6.0")
+
+# Compile Our Solidity
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get byecode
+bytecode = compiled_sol["contracts"][""]
