7ac751cf0d7150825d06e81fa574144f6986b79e
Zachary <zbutler46@hotmail.com>
2023-06-11 18:08:13 -0400

    aded testnet 3rd-party use to non-brownie use code

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -39,10 +39,18 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-# for connecting to ganache
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 1337
-my_address = "0x1B3422E61d4d1a32De7BA390487F0771Ad4aE737"
+# for connecting to ganache local blockchain
+# w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+# chain_id = 1337
+# my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# private_key = os.getenv("PRIVATE_KEY")
+
+# for connecting to 3rd party chain
+w3 = Web3(
+    Web3.HTTPProvider("https://sepolia.infura.io/v3/e19872112bce43e1912a1bc08cf948e0")
+)
+chain_id = 11155111
+my_address = "0x7368F408E4F837403890A6A3bBc3cFC4E2c88441"
 private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
@@ -60,4 +68,38 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-print(signed_txn)
+# Send the transaction
+print("Deploying K...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+print(tx_hash)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(tx_receipt)
+print("Deployed")
+
+# Working with the K, you always need
+# K Address
+# K ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# sometimes people will create an abi file e.g abi.json or abi.py
+# 2 ways to make a transaction with a blockchain
+# 1. Call - simulate a transaction on a blockchain but do not make a state change
+# 2. Transaction - Acutally make a state change (building & sending a transaction)
+
+# Intial value of favorite number
+print(simple_storage.functions.retrieve().call())
+print("Updating K...")
+print(simple_storage.functions.store(15).call())
+
+# if we want to store something using the transaction then we have to go thru the same process as when we deployed the txn
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+
+print(simple_storage.functions.retrieve().call())
+print("Updated!")
